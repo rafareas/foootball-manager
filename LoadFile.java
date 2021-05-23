6e1954949c62fce1fcac03c79317727bb5010181
pedroarauj09 <pedro.a.araujo@outlook.com>
2021-05-23 19:48:45 +0100

    começo do load

--- LoadFile.java
+++ LoadFile.java
@@ -1,6 +1,14 @@
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 public class LoadFile {
-    public void parse(){
+    public Team parse(){
         List<String> linhas = lerFicheiro("dados.csv");
+        Team time = new Team();
         String[] linhaPartida;
         for (String linha : linhas) {
             linhaPartida = linha.split(":", 2);
@@ -9,13 +17,16 @@ public class LoadFile {
                     GuardaRedes j = parseGuardaRedes(linhaPartida[1]);
                     System.out.println(j.toString());
                     break;
-                (...)
+                case "Defesa":
+                case "Medio":
+                case "Avancado":
                 default:
                     System.out.println("Linha invÃ¡lida.");
                     break;
             }
         }
         System.out.println("done!");
+        return time;
     }
 
     public List<String> lerFicheiro(String nomeFich) {
@@ -29,9 +40,9 @@ public class LoadFile {
     public GuardaRedes parseGuardaRedes(String input){
         String[] campos = input.split(",");
         String nome = campos[0];
-        Int numCamisola = Integer.parseInt(campos[1]);
-        Int velocidade = Integer.parseInt(campos[2]);
-        (...)
-        return new GuardaRedes(...);
+        int numCamisola = Integer.parseInt(campos[1]);
+        int velocidade = Integer.parseInt(campos[2]);
+        
+        return new GuardaRedes();
     }   
 }
